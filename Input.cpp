5ea48dc9ef965b789aae3647e15e810dae2db246
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-10-07 16:31:50 +0900

    確認課題02_03

--- Input.cpp
+++ Input.cpp
@@ -8,7 +8,6 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 	HRESULT result;
 
 	//DirectInputのインスタンスを生成
-	ComPtr<IDirectInput8>directInput = nullptr;
 	result = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
 	assert(SUCCEEDED(hr));
 
@@ -27,9 +26,26 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
+	//前回のキー入力を保存
+	memcpy(preKey, key, sizeof(key));
 	//キーボード情報の取得
 	keyboard->Acquire();
 	//全キーボード入力情報を取得
-	BYTE key[256] = {};
 	keyboard->GetDeviceState(sizeof(key), key);
+}
+
+bool Input::PushKey(BYTE keyNumber)
+{
+	if (key[keyNumber]) {
+		return true;
+	}
+	return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+	if (key[keyNumber] && !preKey[keyNumber]) {
+		return true;
+	}
+	return false;
 }
--- Input.h
+++ Input.h
@@ -9,11 +9,19 @@ class Input
 
 public:
 	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 	//初期化
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 	//更新
 	void Update();
 
+	//キーの状態
+	bool PushKey(BYTE keyNumber);
+	bool TriggerKey(BYTE keyNumber);
+
 private:
+	BYTE key[256] = {};
+	BYTE preKey[256] = {};
 	ComPtr<IDirectInputDevice8>keyboard;
+	ComPtr<IDirectInput8>directInput;
 };
