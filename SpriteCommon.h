e2063bf5016050cb02ebb9a66824edd842950199
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-11-18 14:41:19 +0900

    確認課題 05_02 途中

--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "DirectXCommon.h"
 
 class SpriteCommon
 {
@@ -8,5 +9,30 @@ public:
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(DirectXCommon* dxCommon);
+
+	//共通描画設定
+	void CommonDraw();
+
+	DirectXCommon* GetDxCommon()const { return dxCommon_; }
+
+
+private:
+	//ルートシグネチャの作成
+	void RootSignatureInitialize();
+
+	//グラフィックスパイプライン
+	void GraphicsPipelineInitialize();
+
+private:
+	DirectXCommon* dxCommon_;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState = nullptr;
+
+	//バッファリソース
+	//Sprite用の頂点リソースを作る
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = dxCommon_->CreateBufferResource(sizeof(VertexData) * 4);
+	
+	//Sprite用のindexResourceを作成								  
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResource = dxCommon_->CreateBufferResource(sizeof(uint32_t) * 6);
 };
