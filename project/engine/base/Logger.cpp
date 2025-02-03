e484fd1c1ae0f6f39518775d91b17cd62fbf2b1f
OKUBO-0 <k023g0020@g.neec.ac.jp>
2025-02-03 16:44:16 +0900

    09_01 確認課題

--- project/engine/base/Logger.cpp
+++ project/engine/base/Logger.cpp
@@ -4,6 +4,9 @@
 namespace Logger 
 {
 	void Logger::Log(const std::string& message){
+		OutputDebugStringA("文字列リテラルを出力するよ\n");
+
+		std::string a("stringに埋め込んだ文字列を出力するよ\n");
 		OutputDebugStringA(message.c_str());
 	}
 }
